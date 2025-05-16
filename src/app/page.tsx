16f438d12fac76394e74c36d64d946eb7cd0228b
rolevax <rolevax@outlook.com>
2025-05-16 18:12:26 +0800

    add appbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,28 +6,40 @@ import Link from '@mui/material/Link';
 import NextLink from 'next/link';
 import ProTip from '@/components/ProTip';
 import Copyright from '@/components/Copyright';
+import { AppBar, Button, IconButton, Toolbar } from '@mui/material';
+import { Menu } from '@mui/icons-material';
 
 export default function Home() {
   return (
     <Container maxWidth="lg">
-      <Box
-        sx={{
-          my: 4,
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}
-      >
-        <Typography variant="h4" component="h1" sx={{ mb: 2 }}>
-          Material UI - Next.js App Router example in TypeScript
-        </Typography>
-        <Link href="/about" color="secondary" component={NextLink}>
-          Go to the about page
-        </Link>
-        <ProTip />
-        <Copyright />
+      <Box sx={{ width: "100%", flexGrow: 1 }}>
+        <HomeAppBar />
       </Box>
     </Container>
+  );
+}
+
+function HomeAppBar() {
+  return (
+    <AppBar position="sticky">
+      <Toolbar>
+        <IconButton
+          size="large"
+          edge="start"
+          color="inherit"
+          aria-label="menu"
+          sx={{ mr: 2 }}
+        >
+          <Menu />
+        </IconButton>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          Buy Say Sell
+        </Typography>
+        <Button component={NextLink} href="/about">
+          About
+        </Button>
+        {/* <ConnectButton /> */}
+      </Toolbar>
+    </AppBar>
   );
 }
