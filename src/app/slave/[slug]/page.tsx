c1fdd286cb817bffc846035c4cf859e95a859993
rolevax <rolevax@outlook.com>
2025-05-17 01:27:59 +0800

    add slave page input box

--- src/app/slave/[slug]/page.tsx
+++ src/app/slave/[slug]/page.tsx
@@ -5,14 +5,22 @@ import { ArrowBack } from "@mui/icons-material";
 import {
   AppBar,
   Box,
+  Button,
   Container,
+  FormControl,
+  Grid,
   IconButton,
+  Input,
+  InputAdornment,
+  InputLabel,
   List,
   ListItem,
   ListItemText,
+  TextField,
   Toolbar,
   Typography,
 } from "@mui/material";
+import { Butterfly_Kids } from "next/font/google";
 import { usePathname } from "next/navigation";
 
 export default function Slave() {
@@ -57,6 +65,8 @@ function SlaveBody() {
     <Box>
       <SlaveBrief showSlavePageButton={false} />
       <ChatList />
+      <OwningInput />
+      <Box minHeight={200} />
     </Box>
   );
 }
@@ -71,4 +81,160 @@ function ChatList() {
     );
   }
   return <List>{items}</List>;
+}
+
+function SelfNotSlaveInput() {
+  return (
+    <Box>
+      <Grid
+        container
+        spacing={2}
+        direction="row"
+        justifyContent="start"
+        alignItems="center"
+      >
+        <FormControl sx={{ m: 1 }} variant="standard">
+          <InputLabel htmlFor="standard-adornment-amount">Price</InputLabel>
+          <Input
+            id="standard-adornment-amount"
+            startAdornment={<InputAdornment position="start">$</InputAdornment>}
+            // value={props.price}
+            // onChange={(e) => {
+            //   let s = e.target.value;
+            //   props.onPriceChanged?.(BigInt(s));
+            // }}
+            // disabled={!props.onPriceChanged}
+          />
+        </FormControl>
+        <Button
+          variant="contained"
+          type="submit"
+          // disabled={props.isPending}
+        >
+          Be a Slave
+        </Button>
+      </Grid>
+    </Box>
+  );
+}
+
+function SelfSlaveInput() {
+  return (
+    <Box>
+      <TextField
+        label="Answer"
+        fullWidth
+        sx={{ pb: 1 }}
+        // rows={2}
+        // value={props.content}
+        // onChange={(e) => props.onContentChanged?.(e.target.value)}
+        // disabled={!props.onContentChanged}
+      />
+      <Button variant="contained">Send</Button>
+    </Box>
+  );
+}
+
+function OwningInput() {
+  return (
+    <Box>
+      <Typography variant="h6" marginTop={3} marginBottom={1}>
+        Asking
+      </Typography>
+      <TextField
+        label="Question"
+        fullWidth
+        sx={{ pb: 1 }}
+        // rows={2}
+        // value={props.content}
+        // onChange={(e) => props.onContentChanged?.(e.target.value)}
+        // disabled={!props.onContentChanged}
+      />
+      <Button variant="contained">Send</Button>
+      <Typography variant="h6" marginTop={3} marginBottom={1}>
+        Selling
+      </Typography>
+      <Grid
+        container
+        spacing={2}
+        direction="row"
+        justifyContent="start"
+        alignItems="center"
+      >
+        <FormControl sx={{ m: 1 }} variant="standard">
+          <InputLabel htmlFor="standard-adornment-amount">Price</InputLabel>
+          <Input
+            id="standard-adornment-amount"
+            startAdornment={<InputAdornment position="start">$</InputAdornment>}
+            // value={props.price}
+            // onChange={(e) => {
+            //   let s = e.target.value;
+            //   props.onPriceChanged?.(BigInt(s));
+            // }}
+            // disabled={!props.onPriceChanged}
+          />
+        </FormControl>
+        <Button
+          variant="contained"
+          type="submit"
+          // disabled={props.isPending}
+        >
+          Sell
+        </Button>
+      </Grid>
+    </Box>
+  );
+}
+
+function NotOwningInput() {
+  return (
+    <Box>
+      <Typography variant="h6" marginTop={3} marginBottom={1}>
+        Asking
+      </Typography>
+      <TextField
+        label="Question"
+        fullWidth
+        sx={{ pb: 1 }}
+        // rows={2}
+        // value={props.content}
+        // onChange={(e) => props.onContentChanged?.(e.target.value)}
+        disabled={true}
+      />
+      <Button variant="contained" disabled={true}>
+        Send
+      </Button>
+      <Typography variant="h6" marginTop={3} marginBottom={1}>
+        Buying
+      </Typography>
+      <Grid
+        container
+        spacing={2}
+        direction="row"
+        justifyContent="start"
+        alignItems="center"
+      >
+        <FormControl sx={{ m: 1 }} variant="standard">
+          <InputLabel htmlFor="standard-adornment-amount">Price</InputLabel>
+          <Input
+            id="standard-adornment-amount"
+            startAdornment={<InputAdornment position="start">$</InputAdornment>}
+            // value={props.price}
+            // onChange={(e) => {
+            //   let s = e.target.value;
+            //   props.onPriceChanged?.(BigInt(s));
+            // }}
+            disabled={true}
+          />
+        </FormControl>
+        <Button
+          variant="contained"
+          type="submit"
+          // disabled={props.isPending}
+        >
+          Buy
+        </Button>
+      </Grid>
+    </Box>
+  );
 }
